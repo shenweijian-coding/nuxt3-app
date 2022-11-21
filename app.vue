ea2366abcdceb0f0d76ad9b28b5a4fe09c5dad58
entner <wangyi@zhufaner.com>
2022-11-21 14:32:29 +0800

    项目初始化

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>nuxt3 app</h1>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>index page</div>
+  <!--跳转链接-->
+  <NuxtLink to="/detail">Detail Page</NuxtLink>
+</template>
